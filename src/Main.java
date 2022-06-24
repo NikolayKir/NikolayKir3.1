05d959fb3fd4a2f0ad2dd6a13ad4a8dbfa892bf2
NikolayKir <jahberith@gmail.com>
2022-06-24 17:30:16 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusMilesService service = new BonusMilesService();
+        System.out.println(service.calculate(10000));
+    }
+}
